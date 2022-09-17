d18709d87143dd6130924457ce4020417dabb6c1
Lintao <Lintao.Amons@outlook.com>
2022-09-17 03:12:35 -0700

    配置快捷键

--- my-mappings.lua
+++ my-mappings.lua
@@ -9,7 +9,19 @@ return {
     ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>wl"] = { "<cmd>vsplit<cr>", desc = "Split window vertically" },
+    ["<leader>wo"] = { "<c-w>o", desc = "Maximize window" },
+
+    ["<leader>fk"] = { "<cmd>Telescope keymaps<cr>", desc = "Find keymapgs" },
+    ["<C-e>"] = { "<cmd>Telescope oldfiles<cr>", desc = "Find recent files" },
+    ["<leader>xx"] = { '<cmd>lua vim.notify("haha")<cr>', desc = "Test lua mapping" },
+    ["<M-n>"] = { '<cmd>lua vim.notify("heihei")<cr>', desc = "Test lua mapping" },
   },
+  i = {
+    [";;"] = { ":=" },
+    [";a"] = { "!=" },
+  },
+  v = {},
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
